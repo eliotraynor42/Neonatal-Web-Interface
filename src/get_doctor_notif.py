e10524a40086a23e96e2f383cf153669dcad135a
Eliot <emrtiger42@gmail.com>
2025-12-10 02:26:23 -0500

    Fixed get scripts PLEASE PULL
    Updated all of the get_*.py scripts, which broke again since someone pushed the older versions.

--- src/get_doctor_notif.py
+++ src/get_doctor_notif.py
@@ -11,19 +11,19 @@ import json
 #Retrieving information from the database can be done through SQL queries by
 #connecting to the database with the sqlite3 package. 
 def get_doctor_notif(id_doctor):
-    conn = sqlite3.connect("patient_database.db")
+    conn = sqlite3.connect("NWI_DB.db")
     cur = conn.cursor()
 
     # Selection statement to obtain new notifications for this doctor.
     sql = """
-        SELECT id_doctor, message, date, is_read
-        FROM Neonatal_Sample_Dataset
-        WHERE id_doctor = ? AND is_read = 0
+        SELECT id_doctor, message, date, id_patient
+        FROM comms
+        WHERE id_doctor = ?
         ORDER BY date DESC
     """
     
     # Run the SQL query and get the results.
-    cur.execute(sql, (id_doctor,))
+    cur.execute(sql, (id_doctor))
     rows = cur.fetchall()
     conn.close()
     
@@ -33,20 +33,22 @@ def get_doctor_notif(id_doctor):
             "id_doctor": r[0],
             "message": r[1],
             "date": r[2],
-            "is_read": r[3],
+            "id_patient": r[3],
         }
         for r in rows
     ]
 
-    return {"notifications": notifications}
+    return notifications
+
+print(get_doctor_notif("1"))
 
 #Next the function return needs to be turned into a command‑line script that can be called from 
 #a PHP script. Since PHP does not understand Python dict data types, the Python return needs
 #to be converted into a JSON which is a standardized format that can be decoded
 #with PHP for a html script. 
-if __name__ == "__main__":
-    #checks whether the file is being run as a script
-    id_doctor = sys.argv[1]
-    result = get_doctor_notif(id_doctor)
-    print(json.dumps(result))
-    #shell_exec() in PHP can capture the printed text as a string. 
+# if __name__ == "__main__":
+#     #checks whether the file is being run as a script
+#     id_doctor = sys.argv[1]
+#     result = get_doctor_notif(id_doctor)
+#     print(json.dumps(result))
+#     #shell_exec() in PHP can capture the printed text as a string. 
