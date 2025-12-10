b11b5dc61b342d8631c642a3f79d02dfcae8ce88
CometBlades <alexandra.e.nickel@gmail.com>
2025-12-10 06:00:38 -0500

    Bad FIx for submit health

--- src/submit_health.py
+++ src/submit_health.py
@@ -13,7 +13,7 @@ from bmes_ahmet_loader import *
 def submit_patient_data(patient_id,vals):
     # Connect to the database
     conn = sqlite3.connect(bmes.userdownloaddir() + "/NWI_DB.db")
-    db = conn.cursor()
+    conn.cursor()
 
     #Reference variables for calling database
     reference = ['weight_kg', 'length_cm', 'head_circumference_cm', 'temperature_c', 'heart_rate_bpm', 
@@ -25,19 +25,38 @@ def submit_patient_data(patient_id,vals):
     #Gets the new age in days
     birthdate=datetime.date.fromisoformat(data[5]) #Takes the birth date and turns from text into a format a date object
     today=datetime.date.today() #Gets todays date
-    new_age=(today-birthdate).days #Gets the age in days of the patient
+    age_days=(today-birthdate).days #Gets the age in days of the patient
 
 
    #==========Update Patient Data==========#
-    call="INSERT INTO patient_visit_data(id_patient, age_days," #First part of the call
-    for i in range(len(reference)-1): #Function makes the iteative part of the call
-        call +=reference[i] + ", "
-    
-    call +=reference[9] +") VALUES (" +str(patient_id) +", " +str(new_age) +", "
-    for i in range(len(vals)-1):
-        call +=str(vals[i]) +" ,"
-    
-    call += str(vals[9]) +");"
-
-    conn.execute(call)
+    conn.execute(f"""INSERT INTO patient_visit_data (
+                    id_patient,
+                    date,
+                    age_days,
+                    weight_kg,
+                    length_cm,
+                    head_circumference_cm,
+                    temperature_c,
+                    heart_rate_bpm,
+                    respiratory_rate_bpm,
+                    oxygen_saturation,
+                    primary_feeding_type,
+                    feeding_frequency_per_day,
+                    urination_count_per_day,
+                    stool_count_per_day
+                    ) VALUES (
+                    {patient_id},
+                    '{today}',
+                    {age_days},
+                    {vals[0]},
+                    {vals[1]},
+                    {vals[2]},
+                    {vals[3]},
+                    {vals[4]},
+                    {vals[5]},
+                    {vals[6]},
+                    {vals[7]},
+                    {vals[8]},
+                    {vals[9]}
+                    );""")
     conn.close()
